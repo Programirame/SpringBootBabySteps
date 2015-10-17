ace91eaaceac2552e57105a398f1b0021ef559ea
igstojanovski <igor.stojanovski@endava.com>
2015-10-17 12:33:00 +0200

    Basic RestController with two methods mapped to PUT and GET HTTP requests

---
+++ src/main/java/org/baby/steps/controllers/HelloController.java
@@ -0,0 +1,21 @@
+package org.baby.steps.controllers;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController(value = "/example/handlers")
+public class HelloController {
+
+    @RequestMapping(value = "/firstMethodHandler", method = RequestMethod.POST)
+    public String simpleHelloMethodPost() {
+
+        return "Hello World: ";
+    }
+
+    @RequestMapping(value = "/firstMethodHandler", method = RequestMethod.GET)
+    public String simpleHelloMethodGet() {
+
+        return "Hello World: ";
+    }
+}
